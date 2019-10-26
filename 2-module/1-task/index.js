a8439985b35b0ecc027a5376e53a4471c3243fa8
innerhard <innerhard@gmail.com>
2019-10-26 22:40:38 +0300

    Решение первой задачи

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,5 +3,10 @@
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
+let user = {
+  whatYouNedd: 'YesIAm',
+};
 function clone(obj) {
+  return Object.assign({}, obj);
 }
+console.log(clone(user));
