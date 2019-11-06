8055a54a396ceb9ae3f6bf902cd9173166dc90d5
Aleksandr Soshnikov <innerhard@gmail.com>
2019-11-07 00:52:29 +0300

    задача

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,5 +3,15 @@
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
+let objectThis = {};
 function clone(obj) {
+  for (i in obj) {
+    if (typeof obj[i] === 'object') {
+      objectThis[i] = clone(obj[i]);
+    } else {
+      objectThis[i] = obj[i];
+    }
+  }
+  return objectThis;
 }
+
