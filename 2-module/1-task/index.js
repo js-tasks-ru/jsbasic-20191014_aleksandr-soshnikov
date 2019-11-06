007c733d3190aca5efaad7b5a29d41c1ef621576
Aleksandr Soshnikov <innerhard@gmail.com>
2019-11-07 01:32:37 +0300

    Null

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,10 +3,14 @@
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
+
 let objectThis = {};
 function clone(obj) {
   for (i in obj) {
-    if (typeof obj[i] === 'object') {
+    if (obj[i] === null) {
+      objectThis[i] = null;
+    } 
+    else if (typeof obj[i] === 'object') {
       objectThis[i] = clone(obj[i]);
     } else {
       objectThis[i] = obj[i];
@@ -14,4 +18,3 @@ function clone(obj) {
   }
   return objectThis;
 }
-
