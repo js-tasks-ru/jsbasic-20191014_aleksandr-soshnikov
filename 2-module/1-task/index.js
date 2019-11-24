dc9b0a1c3f4fd857be19d32acca51dd87857812d
Aleksandr Soshnikov <innerhard@gmail.com>
2019-11-24 17:43:00 +0300

    Исправленные проверки

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -3,18 +3,16 @@
  * @param {Object} obj - клонируем объект
  * @returns {Object}
  */
-
-let objectThis = {};
-function clone(obj) {
-  for (i in obj) {
-    if (obj[i] === null) {
-      objectThis[i] = null;
-    } 
-    else if (typeof obj[i] === 'object') {
-      objectThis[i] = clone(obj[i]);
+let clone = function(obj) {
+  let newObject = {};
+  for (let property in obj) {
+    if (typeof obj[property] !== 'object' || obj[property] === null) {
+      newObject[property] = obj[property];
+    } else if (Array.isArray(obj[property]) === true) {
+      newObject[property] = obj[property].slice();
     } else {
-      objectThis[i] = obj[i];
+      newObject[property] = clone(obj[property]);
     }
   }
-  return objectThis;
-}
+  return newObject;
+};
